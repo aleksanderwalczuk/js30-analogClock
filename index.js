b561d8f9eada07b5ed8184158231a76024e0998c
rednaxela700 <ohleg@windowslive.com>
2020-01-30 14:33:53 +0100

    index.js: animating clock hands on time change

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ function countTime() {
   const hours = today.getHours();
   const minutes = today.getMinutes();
   const seconds = today.getSeconds();
+
+  secondsHand.style.transform = `rotate(${seconds * 6}deg)`;
+  minutesHand.style.transform = `rotate(${minutes * 6}deg)`;
+  hoursHand.style.transform = `rotate(${hours * 6}deg)`;
   console.log(hours + ":" + minutes + ":" + seconds);
 }
 const t = setInterval(countTime, 1000);
